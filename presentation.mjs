736bd6f6591af736233301868a7fda4d90a21458
Hermes Sandoval Moreno <hsandoval@tides.cl>
2021-11-17 12:14:57 -0300

    Initial Project

---
+++ presentation.mjs
@@ -0,0 +1,93 @@
+import boxen from "boxen";
+import chalk from "chalk";
+import inquirer from "inquirer";
+import clear from "clear";
+import open from "open";
+
+const prompt = inquirer.createPromptModule();
+
+clear();
+
+const questions = [
+    {
+        type: "list",
+        name: "action",
+        message: "What you want to do?",
+        choices: [
+            {
+                name: `Send me an ${chalk.green.bold("email")}?`,
+                value: () => {
+                    open("mailto:hsandoval@tides.cl");
+                    console.log("\nDone, see you soon at inbox.\n");
+                }
+            },
+            {
+                name: 'Show Resume',
+                value: () => {
+                    open('https://github.com/hsandovaltides')
+                } 
+            },
+            {
+                name: "Just quit.",
+                value: () => {
+                    console.log("Hasta la vista.\n");
+                }
+            }
+        ]
+    }
+];
+
+const data = {
+    name: chalk.bold.green("             Hermes Sandoval Moreno"),
+    handle: chalk.white("@hsandovatides"),
+    work: `${chalk.white("Software Engineer at")} ${chalk
+        .hex("#2b82b2")
+        .bold("TIDES SPA")}`,
+    github: chalk.gray("https://github.com/") + chalk.green("hsandovaltides"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("hsandovalmoreno"),
+    npx: chalk.red("npx") + " " + chalk.white("hsandovaltides"),
+    labelWork: chalk.white.bold("       Work:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelCard: chalk.white.bold("       Card:")
+};
+
+const me = boxen(
+    [
+        `${data.name}`,
+        ``,
+        `${data.labelWork}  ${data.work}`,
+        `${data.labelGitHub}  ${data.github}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
+        `${data.labelCard}  ${data.npx}`,
+        ``,
+        `${chalk.italic(
+            "I am currently looking for new opportunities,"
+        )}`,
+        `${chalk.italic("my inbox is always open. Whether you have a")}`,
+        `${chalk.italic(
+            "question or just want to say hi, I will try "
+        )}`,
+        `${chalk.italic(
+            "my best to get back to you!"
+        )}`
+    ].join("\n"),
+    {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: "single",
+        borderColor: "green"
+    }
+);
+
+console.log(me);
+const tip = [
+    `Tip: Try ${chalk.cyanBright.bold(
+        "cmd/ctrl + click"
+    )} on the links above`,
+    '',
+].join("\n");
+console.log(tip);
+
+prompt(questions).then(answer => answer.action());
